bbeec2479a973b978b572d634464ee619a8d5aa3
Fastumix <145198220+Fastumix@users.noreply.github.com>
2024-10-13 10:14:45 +0200

    Added full done main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import './page.module.css'
 import Header from '@/components/main/header/header'
 import Favorite from '@/components/main/favorite/favorite'
 import Recomendation from '@/components/main/recomendations/recomendation'
+import Footer from '@/components/footer/footer'
 export default function page() {
   return (
       <div>
         <Header/>
         <Favorite/>
         <Recomendation/>
+        <Footer/>
       </div>
     )
   }
--- src/components/main/favorite/favorite.tsx
+++ src/components/main/favorite/favorite.tsx
@@ -1,37 +1,86 @@
+'use client';
+
 import React from 'react';
-import './favorite.styles.css'
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+import './favorite.styles.css';
 
 export default function Favorite() {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 6000 })]);
+
   return (
-      <section className='favorite'>
-        <img  className='favorite-img' src="https://storage.concert.ua/JTR/27/aK/66cdc4cd67ed9/7edd.jpg:31-eventpage-main_banner-desktop2x" alt="" />
-        
-        <div className='favorite-wrapper'>
-            <div className='favorite-block-container'>
+    <section className='favorite'>
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+
+          <div className="embla__slide">
+            <img className='favorite-img' src="https://storage.concert.ua/JTR/27/aK/66cdc4cd67ed9/7edd.jpg:31-eventpage-main_banner-desktop2x" alt="" />
+            <div className='favorite-wrapper'>
+              <div className='favorite-block-container'>
                 <div className='favorite-trending-block'>
-                    <p>Trending</p>
+                  <p>Trending</p>
                 </div>
                 <div className='favorite-block'>
-                    <h2>Name</h2>
+                  <h2>Name</h2>
                 </div>
                 <div className='favorite-date_info-container'>
-                    <p>
-                        Date
-                    </p>
-                    <p>
-                        Info
-                    </p>
+                  <p>Date</p>
+                  <p>Info</p>
                 </div>
+              </div>
+              <div className='favorite-button-container'>
+                <button id='button1'>Придбати</button>
+                <button id='button2'>Детальніше</button>
+              </div>
             </div>
-            <div className='favorite-button-container'>
-                <button id='button1'>
-                    Придбати
-                </button>
-                <button id='button2'>
-                    Детальніше
-                </button>
+          </div>
+
+          <div className="embla__slide">
+            <img className='favorite-img' src="https://storage.concert.ua/JTR/27/aK/66cdc4cd67ed9/7edd.jpg:31-eventpage-main_banner-desktop2x" alt="" />
+            <div className='favorite-wrapper'>
+              <div className='favorite-block-container'>
+                <div className='favorite-trending-block'>
+                  <p>Trending</p>
+                </div>
+                <div className='favorite-block'>
+                  <h2>Name</h2>
+                </div>
+                <div className='favorite-date_info-container'>
+                  <p>Date</p>
+                  <p>Info</p>
+                </div>
+              </div>
+              <div className='favorite-button-container'>
+                <button id='button1'>Придбати</button>
+                <button id='button2'>Детальніше</button>
+              </div>
             </div>
+          </div>
+
+          <div className="embla__slide">
+            <img className='favorite-img' src="https://storage.concert.ua/JTR/27/aK/66cdc4cd67ed9/7edd.jpg:31-eventpage-main_banner-desktop2x" alt="" />
+            <div className='favorite-wrapper'>
+              <div className='favorite-block-container'>
+                <div className='favorite-trending-block'>
+                  <p>Trending</p>
+                </div>
+                <div className='favorite-block'>
+                  <h2>Name</h2>
+                </div>
+                <div className='favorite-date_info-container'>
+                  <p>Date</p>
+                  <p>Info</p>
+                </div>
+              </div>
+              <div className='favorite-button-container'>
+                <button id='button1'>Придбати</button>
+                <button id='button2'>Детальніше</button>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </section>
-    )
+      </div>
+    </section>
+  );
 }
